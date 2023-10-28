68d021125ee9b6918288b4e7411745aa12aad4b3
Isaac Salvador <isalva2@uic.edu>
2023-10-28 01:27:18 -0500

    Working

--- network/test_net.py
+++ network/test_net.py
@@ -3,27 +3,44 @@ import matplotlib.pyplot as plt
 
 def test_net():
     
-    # instantiate null graph
+    # instantiate null directed graph
     network = nx.DiGraph()
     
     # initialize intersections
     intersections = {
-        "A": (0, 0),
-        "B": (2, 0),
-        "C": (0, 2),
-        "D": (2, 2)
+        "A": {"pos": (0, 0)},
+        "B": {"pos": (2, 0)},
+        "C": {"pos": (0, 2)},
+        "D": {"pos": (2, 2)}
     }
-    
+
     # add intersections
-    network.add_nodes_from(intersections)
+    for node, attrs in intersections.items():
+        network.add_node(node, **attrs)
     
     # intialize roads
-    roads = [("A", "B"), ("B", "D"), ("A", "C"), ("C", "D")]
-    
+    roads = [
+        ("A", "B", {"length": 2}),
+        ("B", "D", {"length": 2}),
+        ("A", "C", {"length": 2}),
+        ("C", "D", {"length": 2})
+    ]
+
     # add roads
     network.add_edges_from(roads)
     
     return network
 
 if __name__ == "__main__":
-    pass
+    
+    # load test net
+    network = test_net()
+    
+    # get position of nodes
+    pos = nx.get_node_attributes(network, "pos")
+    
+    # graph network
+    plt.figure(figsize=(6,6))
+    nx.draw(network, pos, with_labels=True)
+    plt.axis('off')
+    plt.show()
