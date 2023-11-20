fe077814950cde2ff72595d51f7e8897b5d5333d
Ahreum Kim <akim239@uic.edu>
2023-11-20 12:19:58 -0600

    update step/reset fn, add trunctaion condition

--- models/model_3/API_test.py
+++ models/model_3/API_test.py
@@ -1,12 +1,15 @@
 from pettingzoo.test import parallel_api_test, api_test
 
-import RoadNetEnv3
+# import RoadNetEnv3
+import RoadNetEnv3_latency_test
 
-env = RoadNetEnv3.parallel_env()
+# env = RoadNetEnv3.parallel_env()
+env = RoadNetEnv3_latency_test.parallel_env()
 
 parallel_api_test(env, num_cycles=1000)
 
-env = RoadNetEnv3.raw_env()
+# env = RoadNetEnv3.raw_env()
+env = RoadNetEnv3_latency_test.parallel_env()
 
 api_test(env, num_cycles=1000)
 
