e9060adfcf7753bf34b634b15cff22a49ee86332
Isaac Salvador <isalva2@uic.edu>
2023-11-19 22:03:49 -0600

    Added debuggin utils

--- models/model_3/Reset_test.py
+++ models/model_3/Reset_test.py
@@ -1,6 +1,6 @@
 from pettingzoo.test import parallel_api_test
 
-import RoadNetEnv3 as rne
+import RoadNetEnv3_debugging as rne
 
 env = rne.parallel_env()
 
@@ -11,12 +11,13 @@ test_action = {agent_id: 0 for agent_id in env.agents}
 # print("State before reset")
 print(env.state())
 
-for _ in range(10):
+for _ in range(100):
     env.step(test_action)
+    print(env.state())
 
 print("\n\n\n\n")
 # print("State after step")
-# print(env.state())
+#print(env.state())
 
 state, _ = env.reset()
 
