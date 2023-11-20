750128c5cdbd7173cbb1ad5f175ddbb9a64f6a05
Isaac Salvador <isalva2@uic.edu>
2023-11-19 22:54:39 -0600

    Debugging

--- models/model_3/Reset_test.py
+++ models/model_3/Reset_test.py
@@ -9,17 +9,17 @@ state, _ = env.reset()
 test_action = {agent_id: 0 for agent_id in env.agents}
 
 # print("State before reset")
-print(env.state())
+#print(env.state())
 
 for _ in range(100):
     env.step(test_action)
-    print(env.state())
+    #print(env.state())
 
 print("\n\n\n\n")
 # print("State after step")
-#print(env.state())
+print(env.state())
 
 state, _ = env.reset()
 
 # print("State after reset")
-print(env.state())
+#print(env.state())
